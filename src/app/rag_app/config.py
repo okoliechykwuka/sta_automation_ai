cf296825e0669ebb2701276a25921c8071467e49
neyedhayo <0xmaxlord@gmail.com>
2024-08-01 17:55:43 +0100

    changed enpdpoint to environment variable

--- src/app/rag_app/config.py
+++ src/app/rag_app/config.py
@@ -7,6 +7,6 @@ class Config:
     NEO4J_URI = os.getenv('NEO4J_URI')
     NEO4J_USERNAME = os.getenv('NEO4J_USERNAME')
     NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD')
-    MODEL_ENDPOINT = "https://4b98-102-91-92-8.ngrok-free.app/"  # Replace with your actual model endpoint
+    MODEL_ENDPOINT = os.getenv('MODEL_ENDPOINT') # Replace with your actual model endpoint
 
 config = Config()

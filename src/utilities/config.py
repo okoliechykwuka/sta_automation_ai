ef5dc70e2857bb1a0886f347209db393ae828026
neyedhayo <0xmaxlord@gmail.com>
2024-08-29 18:03:36 +0100

    deleted script files relocated enmass

--- src/utilities/config.py
+++ src/utilities/config.py
@@ -7,6 +7,7 @@ class Config:
     NEO4J_URI = os.getenv('NEO4J_URI')
     NEO4J_USER = os.getenv('NEO4J_USER')
     NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD')
-    MODEL_ENDPOINT = "https://6407-102-91-4-155.ngrok-free.app/"  # Replace with your actual model endpoint
+    MODEL_ENDPOINT = os.getenv('MODEL_ENDPOINT')  # Replace with your actual model endpoint
+    # OPENAI_API_KEY = os.getenv('OPENAI_API_KEY') # newly added for openai
 
 config = Config()

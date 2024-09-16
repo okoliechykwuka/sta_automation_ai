c4861b4373fb1b211d3f710584af3d36adf36018
neyedhayo <0xmaxlord@gmail.com>
2024-09-16 17:10:30 +0100

    updatd config environment

--- src/utilities/config.py
+++ src/utilities/config.py
@@ -11,7 +11,8 @@ class Config:
     MODEL_ENDPOINT = os.getenv('MODEL_ENDPOINT')
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
-config = Config()
+app_config = Config()
+print(f"Loaded OPENAI_API_KEY: {app_config.OPENAI_API_KEY}")
 
 # print(f"Loaded OPENAI_API_KEY: {config.OPENAI_API_KEY}")
-print(f"Loaded MODEL_ENDPOINT: {config.MODEL_ENDPOINT}")
+print(f"Loaded MODEL_ENDPOINT: {app_config.MODEL_ENDPOINT}")
